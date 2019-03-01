d8bf2a31619df7f421c34a09f267f57188256970
Bryan Baird <bryan@kerigan.com>
2019-03-01 16:02:26 -0500

    section title adjustment

--- public/themes/wordplate/views/partials/footer.blade.php
+++ public/themes/wordplate/views/partials/footer.blade.php
@@ -1,6 +1,8 @@
 <footer class="sticky-footer bg-dark py-4">
     <div class="container text-center">
-        <h2>Contact Me</h2>
+        <div class="section-title">
+            <h2>Contact Me</h2>
+        </div>
         <p class="m-0"><a href="tel:{{ get_field('phone', 'option') }}">{{ get_field('phone', 'option') }}</a></p>
         <p><a href="mailto:{{ get_field('email', 'option') }}">{{ get_field('email', 'option') }}</a></p>
         <social-icons :size="37" :margin=".25" class="d-flex social-icons justify-content-center mb-4" ></social-icons>
